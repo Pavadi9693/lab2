b17bb14464c3c8c14ca9f5da4a995b62aad7a936
sandeepa p <bhimasandeep336@gmail.com>
2026-03-17 22:08:41 +0530

    added app files for the website

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        int result = app.add(2,3);
+        Assert.assertEquals(5, result);
+    }
+}
